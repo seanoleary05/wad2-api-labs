49bd4680ec46e254b8b149474369d908f383a6e5
seanoleary05 <20106716@mail.wit.ie>
2025-11-25 12:25:02 +0000

    Added Users Router

--- tasky-api/index.js
+++ tasky-api/index.js
@@ -1,12 +1,23 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import tasksRouter from './api/tasks';
+import usersRouter from './api/users';
+
 import './db';
+
+// other imports
+import cors from 'cors';
+
+
+
 
 
 dotenv.config();
 
-const errHandler = (err, req, res) => {
+// Enable CORS for all requests
+
+
+const errHandler = (err, req, res, next) => {
   /* if the error in development then send stack trace to display whole error,
   if it's in production then just send error message  */
   if(process.env.NODE_ENV === 'production') {
@@ -15,13 +26,22 @@ const errHandler = (err, req, res) => {
   res.status(500).send(`Hey!! You caught the error 👍👍. Here's the details: ${err.stack} `);
 };
 
+
+
+
 const app = express();
 
 const port = process.env.PORT;
 
+app.use(cors());
+
 app.use(express.json());
 
 app.use('/api/tasks', tasksRouter);
+
+//Users router
+app.use('/api/users', usersRouter);
+
 
 app.use(errHandler);
 
