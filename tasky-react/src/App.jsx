901239740d607279f755ecd0bd141eb6134b42be
seanoleary05 <20106716@mail.wit.ie>
2025-12-08 21:39:12 +0000

    Site Header added

--- tasky-react/src/App.jsx
+++ tasky-react/src/App.jsx
@@ -9,6 +9,8 @@ import ProfilePage from "./pages/profilePage";
 import AuthContextProvider from "./contexts/authContext";
 import ProtectedRoutes from "./protectedRoutes";
 import './App.css';
+import Header from "./components/SiteHeader";
+
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -26,7 +28,7 @@ const App = () => {
       <BrowserRouter>
         <AuthContextProvider>
           <div className="container">
-            <h1>Tasky</h1>
+            <Header />
             <Routes>
               <Route path="/" element={< StartPage />} />
               <Route path="/login" element={< LoginPage />} />
